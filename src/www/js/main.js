06112eb2aa54aa6444d2540c4bb63b8d1de39da6
Ryan Stewart <rstewart@adobe.com>
2012-04-17 15:17:45 -0700

    Working on sliding. Only works to the left (merged changes)

--- src/www/js/main.js
+++ src/www/js/main.js
@@ -1,3 +1,38 @@
+var views;
+var elementWidth = 250;
+var numOfElements;
+
+
 $(document).ready(function() {
   $("#set_list").list();
 });
+
+function onBodyLoad() {
+    $("#viewport").click(onViewPortClick);
+    
+    var counter = 0;
+    $("#slidecontainer").children().each(function() {
+        $(this).css("left", counter * $(this).width());
+        counter++;
+    });
+    numOfElements = counter;
+}
+
+function onViewPortClick(event) {
+    var endReached = false;
+    $("#slidecontainer").children().each(function(e) {
+        var leftvalue = parseInt($(this).css("left"),10);
+		console.log(leftvalue);
+        if(e == numOfElements-1 && leftvalue == 0) {
+            endReached = true;
+        }
+        // Adding the animation class here because if I add it onBodyLoad 
+        // it plays when it resizes the container. But this seems kind of
+        // innefficient. 
+        if(!endReached) {
+            $(this).addClass("viewcontaineranimation");
+            $(this).css("left", leftvalue - elementWidth);
+        }
+    });
+}
+
