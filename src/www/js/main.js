9de5b47296bb2bd4f9d9f3703abe76af88c50dcb
Ryan Stewart <rstewart@adobe.com>
2012-04-18 17:16:27 -0700

    Made slide in work for card list

--- src/www/js/main.js
+++ src/www/js/main.js
@@ -5,9 +5,7 @@ var spectralKitten;
 $(document).ready(function() {
 	$("#set_list").list();
     $("#list_container").bind('change',function(e) {
-        $(".listholder").html("Shown");
-        $(".listholder").css("display","block");
-        $(".listholder").css("left",0);
+        // don't think I need this anymore. 
     });
 	$(".nav").click(function(event) {
 		$(".active").removeClass("active");
@@ -55,14 +53,43 @@ $(document).ready(function() {
 	
 });
 
-function renderCardList(cards){
+function renderCardList(cards,series_id){
+    var source = $('#card-list-template').html();
+    var template = Handlebars.compile(source);
+
+    var timestamp = new Date().getTime();
+    var div_id = "series_" + series_id + "_" + timestamp;
+    
+    var context = {"cards":cards, "div_id":div_id};
+    var cardlist = template(context);
+ 
+    
+    $(cardlist).appendTo("#list_holder");
+    $("#list_holder").children().last().list();
+    $("#list_holder").children().last().css("left",0);
+    
+    
+    //$(cardlist).css("left","400px");
+    
+    /*
+    setTimeout(function(){
+        //console.log($(cardlist).css("left"));
+        document.getElementById(div_id).style.left = "0px";
+        console.log($(cardlist).css("left"));
+    }, 1000);    
+    */
+      
+    //$(cardlist).list();    
+    
+    //$(seriesdiv).css("left",0);
+    
 }
 
 function renderSeriesList(series){
 	var source = $('#series-list-template').html();
 	var template = Handlebars.compile(source);
 	
-	var context = {"series": spectralKitten.series};
+	var context = {"series": series};
 	var html = template(context);
 	
 	$('#list_container').html(html);
@@ -74,9 +101,11 @@ function renderSeriesList(series){
 		function( event ){
 			var series_id = $(event.srcElement).data("series_id");
 			var t = new Date().getTime();
-			var series = spectralKitten.getCardsBySet(series_id);
+			var cards = spectralKitten.getCardsBySet(series_id);
+            renderCardList(cards,series_id);
 		}
 	);
+    $("#list_container").css("left",0);
 }
 
 
