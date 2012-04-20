6a779ab718bd8c07d895aded21c2fcf2e4f1a918
Mike Chambers <mikechambers@gmail.com>
2012-04-19 22:54:43 -0700

    fixed bug where card view was being removed at wrong time. closes issue #21

--- src/www/js/main.js
+++ src/www/js/main.js
@@ -76,10 +76,10 @@ function renderDetailTemplate(template, context){
 		"webkitTransitionEnd",
 		function(){
 			if(currentCardDetailView){
-				currentCardDetailView.unbind("webkitTransitionEnd");
 				removeDetailView(currentCardDetailView);
 			}
 			currentCardDetailView = detail;
+			currentCardDetailView.unbind("webkitTransitionEnd");
 		}
 	);		
 	
