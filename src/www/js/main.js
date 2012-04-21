475bcd71fece3973c3e849fb188a4920a9f471ed
Mike Chambers <mikechambers@gmail.com>
2012-04-21 16:55:01 -0700

    have app working using require.js to load files. right now, we are not using modules, just using require to load javascript files, so we can then use its build system

--- src/www/js/main.js
+++ src/www/js/main.js
@@ -1,85 +1,15 @@
-
-;(function(exports) {
+;(function(window) {
 	'use strict';
 
 var views;
 var viewportWidth;
 var spectralKitten;
-
-$(document).ready(function() {
-
-	$(".nav").click(function(event) {
-		$(".active").removeClass("active");
-		$(event.target).parent().addClass("active");
-		slideViewport($(event.target).data().view);
-	});
 	
-    $(".viewcontainer").removeClass("viewhidden");
-    
-    $('#viewport').width($(window).width());
-    $('#viewport').height($(window).height()-40); //height of the header
-    
-    viewportWidth = $("#viewport").width();
+var currentCardDetailView = null;
+var $;
 
-	var numberOfViews = $("#slidecontainer").children().length;
-	$("#slidecontainer").width(numberOfViews * viewportWidth);
-	$("#slidecontainer").children().each(function() {
-		$(this).width(viewportWidth);
-	});
-	    
-    $(window).resize(function() {
-        var win_width = $(window).width();
-        var win_height = $(window).height();
-        
-        $('#viewport').width(win_width);
-        $('#viewport').height(win_height-40); //height of the header
-        
-        viewportWidth = $('#viewport').width();
+var renderDetailTemplate = function(template, context){
 
-        $("#slidecontainer").width(numberOfViews * viewportWidth);
-        $("#slidecontainer").children().each(function() {
-            $(this).width(viewportWidth);
-        });
-        
-        var activeview = $('.active').children().data('view');
-        slideViewport(activeview);
-        
-        // TODO: Fix annoying flash when you resize.
-
-    });
-
-	spectralKitten = new SpectralKitten("/api/");
-	
-	spectralKitten = new SpectralKitten();
-	spectralKitten.apiVersionName = "version.json";
-	spectralKitten.apiCardsName = "all_cards.json";
-	
-	spectralKitten.initializeData(
-		function() {
-			renderSeriesList(spectralKitten.series);
-			
-			/*
-			s.checkForUpdates(
-				function(newDataFound, data){
-					console.log("New data found? : " + newDataFound);
-				},
-				function (error){
-					console.log("error checking for data update");
-				},
-				true //auto update if there is new data
-			);
-			*/
-		},
-		function(error) {
-			console.log('could not initialize data');
-		},
-		false /* force loading of data from server*/
-	);
-	
-});
-
-
-function renderDetailTemplate(template, context){
 	var source = $(template).html();
 	var template = Handlebars.compile(source);
 	var html = template(context);
@@ -109,13 +39,14 @@ function renderDetailTemplate(template, context){
 	
 	return detail;
 }
+
+
 	
-function removeDetailView(view){
+var removeDetailView = function(view){
 	view.remove();
 }
 
-var currentCardDetailView = null;
-function renderCardDetail(card){
+var renderCardDetail = function(card){
 	
 	var rules = SpectralKitten.parseCardRules(card.rules);
 	
@@ -142,34 +73,35 @@ function renderCardDetail(card){
 }
 	
 /*series is a single series item*/
-function renderSeriesDetail(series){
+var renderSeriesDetail = function(series){
 	renderDetailTemplate("#series-detail-template", {"series": series});
 }
 
 //todo : there wont always be a series_id
-function renderCardList(cards, series_id){
-    var source = $('#card-list-template').html();
-    var template = Handlebars.compile(source);
+var renderCardList = function(cards, series_id){
+	var source = $('#card-list-template').html();
+	var template = Handlebars.compile(source);
 
-    var timestamp = new Date().getTime();
-    var div_id = "series_" + series_id + "_" + timestamp;
-    
-    var context = {"cards":cards, "div_id":div_id};
-    var cardlist = template(context);
+	var timestamp = new Date().getTime();
+	var div_id = "series_" + series_id + "_" + timestamp;
+	
+	var context = {"cards":cards, "div_id":div_id};
+	var cardlist = template(context);
  
-    var list = $(cardlist).appendTo("#list_holder");
-    list.bind('change', function(event) {
-        var card_id = $(event.srcElement).data("card_id");
-        var c = spectralKitten.getCard(card_id);
+	var list = $(cardlist).appendTo("#list_holder");
+	list.bind('change', function(event) {
+		var card_id = $(event.srcElement).data("card_id");
+		var c = spectralKitten.getCard(card_id);
 		renderCardDetail(c);
-    });
+	});
 	
 	list.list();
-    list.css("left",0);
+	list.css("left",0);
 }
 
 /* series is an array of series */
-function renderSeriesList(series){
+var renderSeriesList = function(series){
+	
 	var source = $('#series-list-template').html();
 	var template = Handlebars.compile(source);
 	
@@ -186,29 +118,104 @@ function renderSeriesList(series){
 			var series_id = $(event.srcElement).data("series_id");
 			var t = new Date().getTime();
 			var series = spectralKitten.getCardsBySet(series_id);
-            
+			
 			var s = spectralKitten.getSeries(series_id);
 
-            renderCardList(series,series_id);
+			renderCardList(series,series_id);
 			
 			renderSeriesDetail(s);
 		}
 	);
-    $("#list_container").css("left",0);
+	$("#list_container").css("left",0);
 }
 
-
-function slideViewport(index) {
-            
+var slideViewport = function(index) {
+			
 	var pixelsToMove = (((index-1) * viewportWidth)*-1)-1;
 	
-    $("#slidecontainer").css("left", pixelsToMove);
+	$("#slidecontainer").css("left", pixelsToMove);
 	
 }
 
-function onSelectView(event) {
-	
-}
+require(["libs/jquery.min", "js/FileSystemManager.js", "js/SpectralKitten.js", "libs/domReady.min",
+		"js/libs/bootstrap.min.js", "js/libs/bootstrap-list.js", "js/Settings.js", "libs/handlebars"],
+		function(_$, FileManager, SpectralKitten, domReady) {
+			
+			$ = window.$;
+			SpectralKitten = window.SpectralKitten;
+			
+		  domReady.withResources(
+			  function () {
+				$(".nav").click(function(event) {
+					$(".active").removeClass("active");
+					$(event.target).parent().addClass("active");
+					slideViewport($(event.target).data().view);
+				});
+				
+				$(".viewcontainer").removeClass("viewhidden");
+				
+				$('#viewport').width($(window).width());
+				$('#viewport').height($(window).height()-40); //height of the header
+				
+				viewportWidth = $("#viewport").width();
+			
+				var numberOfViews = $("#slidecontainer").children().length;
+				$("#slidecontainer").width(numberOfViews * viewportWidth);
+				$("#slidecontainer").children().each(function() {
+					$(this).width(viewportWidth);
+				});
+					
+				$(window).resize(function() {
+					var win_width = $(window).width();
+					var win_height = $(window).height();
+					
+					$('#viewport').width(win_width);
+					$('#viewport').height(win_height-40); //height of the header
+					
+					viewportWidth = $('#viewport').width();
+			
+					$("#slidecontainer").width(numberOfViews * viewportWidth);
+					$("#slidecontainer").children().each(function() {
+						$(this).width(viewportWidth);
+					});
+					
+					var activeview = $('.active').children().data('view');
+					slideViewport(activeview);
+					
+					// TODO: Fix annoying flash when you resize.
+			
+				});
+				  
+				spectralKitten = new SpectralKitten("/api/");
+				  
+				spectralKitten.apiVersionName = "version.json";
+				spectralKitten.apiCardsName = "all_cards.json";
+				
+				spectralKitten.initializeData(
+					function() {
+						renderSeriesList(spectralKitten.series);
+						
+						/*
+						s.checkForUpdates(
+							function(newDataFound, data){
+								console.log("New data found? : " + newDataFound);
+							},
+							function (error){
+								console.log("error checking for data update");
+							},
+							true //auto update if there is new data
+						);
+						*/
+					},
+					function(error) {
+						console.log('could not initialize data');
+					},
+					false /* force loading of data from server*/
+				);
+			  }
+		  );
+});
 
-}(this));
+}(window));
+
 
